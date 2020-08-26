0c53727f163e5d9ae6b5e46c0e69f34cdb96dcd1
Ronald G Minnich <rminnich@gmail.com>
2020-08-26 12:51:51 -0700

    Add baud rate setting code
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- src/drivers/uart/src/lib.rs
+++ src/drivers/uart/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 #![feature(llvm_asm)]
 
-#[cfg(feature = "log")]
-pub mod log;
 #[cfg(feature = "i8250")]
 pub mod i8250;
+#[cfg(feature = "log")]
+pub mod log;
 #[cfg(feature = "ns16550")]
 pub mod ns16550;
 #[cfg(feature = "null")]
@@ -15,3 +15,8 @@ pub mod opentitan;
 pub mod pl011;
 #[cfg(feature = "sifive")]
 pub mod sifive;
+
+/* Calculate divisor. Do not floor but round to nearest integer. */
+pub fn uart_baudrate_divisor(baudrate: usize, refclk: usize, oversample: usize) -> usize {
+    (1 + (2 * refclk) / (baudrate * oversample)) / 2
+}
