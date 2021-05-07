0c79843dddcb713936503145e408cba1e65b84b6
Daniel Maslowski <info@orangecms.org>
2021-05-08 00:04:30 +0200

    TEMP HACK: strip down common APCI setup

--- src/soc/amd/common/boot/src/acpi.rs
+++ src/soc/amd/common/boot/src/acpi.rs
@@ -45,6 +45,7 @@ pub fn setup_acpi_tables(w: &mut impl core::fmt::Write, start: usize) -> usize {
     write(w, gencsum(rsdp_offset, rsdp_offset + ACPI_RSDP_CHECKSUM_LENGTH), rsdp_offset, ACPI_RSDP_CHECKSUM_OFFSET); // XXX
     debug_assert_eq!(acpi_tb_checksum(rsdp_offset, rsdp_offset + ACPI_RSDP_CHECKSUM_LENGTH), 0);
 
+    /*
     write(w, gencsum(rsdp_offset, rsdp_offset + ACPI_RSDP_XCHECKSUM_LENGTH), rsdp_offset, ACPI_RSDP_XCHECKSUM_OFFSET); // XXX
     debug_assert_eq!(acpi_tb_checksum(rsdp_offset, rsdp_offset + ACPI_RSDP_XCHECKSUM_LENGTH), 0);
 
@@ -176,6 +177,6 @@ pub fn setup_acpi_tables(w: &mut impl core::fmt::Write, start: usize) -> usize {
     };
     write(w, hpet, hpet_offset, 0);
     write(w, gencsum(hpet_offset, hpet_offset + size_of::<AcpiTableHpet>()), hpet_offset, ACPI_TABLE_HEADER_CHECKSUM_OFFSET);
-
+    */
     round_up_4k(total_size)
 }
