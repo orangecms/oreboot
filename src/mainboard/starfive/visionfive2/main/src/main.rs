6233dea731c33103497885fb278e1091cbf97836
Daniel Maslowski <info@orangecms.org>
2024-01-13 22:26:27 +0100

    VF2 FIXUP - CHECKPOINT
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/starfive/visionfive2/main/src/main.rs
+++ src/mainboard/starfive/visionfive2/main/src/main.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, panic::PanicInfo, ptr};
-use core::{
-    arch::{global_asm, naked_asm},
-    panic::PanicInfo,
-};
+use core::arch::{asm, global_asm};
+use core::panic::PanicInfo;
+use core::ptr::{self, addr_of, addr_of_mut};
 use log::{print, println};
 use oreboot_compression::decompress;
 use riscv::register::mhartid;
@@ -81,6 +79,10 @@ pub unsafe extern "C" fn start() -> ! {
         "j      .boothart",
         // wait for multihart to get back into the game
         ".nonboothart:",
+        // "csrw   mie, 8", // 1 << 3
+        "csrw   mie, 0", // disable wakeup
+        "wfi",
+        "csrw   mip, 0",
         "call   {resume}",
 
         ".boothart:",
@@ -108,11 +110,11 @@ pub unsafe extern "C" fn init() {
         static _sidata: u8;
     }
 
-    let count = &_ebss as *const u8 as usize - &_sbss as *const u8 as usize;
-    ptr::write_bytes(&mut _sbss as *mut u8, 0, count);
+    let bss_size = addr_of!(_ebss) as usize - addr_of!(_sbss) as usize;
+    ptr::write_bytes(addr_of_mut!(_sbss), 0, bss_size);
 
-    let count = &_edata as *const u8 as usize - &_sdata as *const u8 as usize;
-    ptr::copy_nonoverlapping(&_sidata as *const u8, &mut _sdata as *mut u8, count);
+    let data_size = addr_of!(_edata) as usize - addr_of!(_sdata) as usize;
+    ptr::copy_nonoverlapping(addr_of!(_sidata), addr_of_mut!(_sdata), data_size);
 }
 
 /// # Safety
