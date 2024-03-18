2e61b2b7add0433921b079b0a5a844284e094a5a
Daniel Maslowski <info@orangecms.org>
2024-03-18 19:52:08 +0100

    vf2/main simplify panic handler
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- src/mainboard/starfive/visionfive2/main/src/main.rs
+++ src/mainboard/starfive/visionfive2/main/src/main.rs
@@ -254,11 +254,9 @@ fn payload(payload_addr: usize) {
 #[cfg_attr(not(test), panic_handler)]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
-        println!(
-            "[main] panic in '{}' line {}",
-            location.file(),
-            location.line(),
-        );
+        let f = location.file();
+        let l = location.line();
+        println!("[main] panic in '{f}' line {l}");
     } else {
         println!("[main] panic at unknown location");
     };
